621e986ca77d2f7890f6785a17720d28a19ff827
BlazingAssassin <damon.j.nunez@gmail.com>
2021-09-28 15:36:16 +0000

    Changes

--- index.js
+++ index.js
@@ -6,30 +6,38 @@ for(var i = 1; i <= 50; i++) {
 }
 
 //2
-function countFromOne(){
-for(var i = 1; i < 12 ;i++){
+function countFromOne(num){
+for(var i = 1; i < num ;i++){
     console.log(i);
 }
  }
 
 
 //3
-function threeAndFiveInFifty(array){
+const threeAndFiveInFifty = () => {
+    for(let i = 1; i < 51; i++) {
+        if(i % 3 === 0 || i % 5 === 0) {
+            console.log(i);
+        }
+    }
 }
 
-
-//4
-function threeAndFive(){
-  
+const threeOrFive = (int) => {
+      for(let i = 1; i < int + 1; i++) {
+        if(i % 3 === 0 || i % 5 === 0) {
+            console.log(i);
+        }
+    }
 }
 //5
-function double(numbers){
+function triple(numbers){
 let output = [];
 for (let x of numbers)
 output.push(x * 3);
  return output;
 }
 
+
+console.log(triple([4,8,10,]));
+console.log(countFromOne(5));
 console.log(countToFifty());
-console.log(double([4,8,10,]));
-console.log(countFromOne());
