977269abcd9887c2735d850c6063b8dfe7ee66fe
BlazingAssassin <damon.j.nunez@gmail.com>
2021-09-28 14:48:17 +0000

    Practice more

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 //1
 function countToFifty(){
-  
+for(var i = 1; i <= 50; i++) {
+    console.log(i);
+}
 }
 
 //2
 function countFromOne(){
-  
+for(var i = 1; i < 12 ;i++){
+    console.log(i);
 }
+ }
+
 
 //3
-function threeAndFiveInFifty(){
-  
+function threeAndFiveInFifty(array){
 }
+
 
 //4
 function threeAndFive(){
   
 }
+//5
+function double(numbers){
+let output = [];
+for (let x of numbers)
+output.push(x * 3);
+ return output;
+}
 
-
+console.log(countToFifty());
+console.log(double([4,8,10,]));
+console.log(countFromOne());
